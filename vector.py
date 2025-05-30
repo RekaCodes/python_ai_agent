b2fb812d1d4d9698d798330aa7b7d2127ba4cff9
RekaCodes <derrickhudsongreen@gmail.com>
2025-05-29 20:51:18 -0400

    updated vector logic

--- vector.py
+++ vector.py
@@ -32,6 +32,4 @@ vector_store = Chroma(
 if add_documents:
     vector_store.add_documents(documents=documents, ids=ids)
 
-retriever = vector_store.as_retriever(
-    search_kwargs={"k": 5}
-)
+retriever = vector_store.as_retriever()
