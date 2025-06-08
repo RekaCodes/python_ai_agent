47ccfe022c3505902787ff8109beaf0031a3ce9d
RekaCodes <derrickhudsongreen@gmail.com>
2025-06-08 10:38:46 -0400

    check for harmful prompts

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
 
+# selection model
 model = OllamaLLM(model="llama3.2")
 
+# create prompt template
 template = """
 You are an expert in student health and well-being. 
 You are interested in understanding what makes students test well, 
@@ -21,18 +23,24 @@ the main factors that correlate with high and low test scores.
 Here is the question to answer: {question}
 """
 
+# create prompt chain
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 
-
+# loop through prompts and return results
 while True:
     print("\n\n--------------------------------")
     question = input("Ask your question (or type 'exit' to quit): ")
     print("\n\n")
+    # strip prompt of any potentially harmful html/js injections
+    question = question.replace("<", "&lt;").replace(">", "&gt;")
+    # exit if user types exit
     if question.lower() == "exit":
         break
+    # retrieve relevant documents from vector store
     scores = retriever.invoke(question)
     result = chain.invoke({"survey_results": scores, "question": question})
-
+    # print the result
     print(result)
+
